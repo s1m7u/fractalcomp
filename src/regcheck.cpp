7c40ea77b27e883f830114546addb6b16388cb0b
Ethan du Toit <e.du_toit@student.unsw.edu.au>
2024-04-21 04:37:25 +1000

    ok having looked at it for a bit idk where the closed form is wrong here. havent done too much will do more tmrw.

--- src/regcheck.cpp
+++ src/regcheck.cpp
@@ -1,18 +1,69 @@
-#include <stdio.h>
+typedef struct precomp {
+    block* domain;
+    block* range;
+    int orient;
+    double* pfxsum;
+} precomp;
 
-#include "tf.h"
-#include "block.h"
+precomp regression_precomp(block* domain, block* range, int orient, tf_data& tf) {
+    //work towards a reusable regression
+    //float ds = (float)blockSum(domain);
+    //float dsq = (float)blockSumOfSq(domain);
 
-tf_collection* tf_collection_create(unsigned int height, unsigned int width) {
-    tf_collection* ret = new tf_collection;
-    ret->height = height;
-    ret->width = width;
-    return ret;
+    float denom = domain->size * domain->size * dsq - ds * ds;
+
+    if(denom == 0) {
+        return 100000;
+    }
+
+    double* pfxsum = malloc(sizeof(double)*domain->size*domain->size);
+    
+    float xy = 0;
+    for(unsigned int i = 0; i < domain->size; i++) {
+        for(unsigned int j = 0; j < domain->size; j++) {
+            unsigned int y = i, x = j;
+            calc_pos(&y, &x, orient, domain->size);
+
+            pfxsum[i * domain->size + j] = yx_to_val(y,x,domain) * yx_to_val(i,j,range);
+        }
+    }
+
+    float rs = (float)blockSum(range);
+
+    tf.contrast = ((float)(domain->size * domain->size * xy) - ds * rs)/denom;
+    /* tf.brightness = (float)(rs * dsq - ds * xy)/denom; */
+    tf.brightness = (float)(rs - tf.contrast * ds)/(domain->size*domain->size);
+    /* printf("%f %f\n", tf.contrast, tf.brightness); */
+    tf.d_block[0] = domain->y;
+    tf.d_block[1] = domain->x;
+    tf.r_block[0] = range->y;
+    tf.r_block[1] = range->x;
+    tf.size = range->size;
+    tf.orient = orient;
+
+    float residual = 0;
+
+    for(unsigned int i = 0; i < domain->size; i++) {
+        for(unsigned int j = 0; j < domain->size; j++) {
+            unsigned int y = i, x = j;
+            calc_pos(&y, &x, orient,domain->size);
+
+            float temp = (yx_to_val(y,x,domain) * tf.contrast + tf.brightness 
+                    - yx_to_val(i,j,range));
+            temp *= temp;
+            residual += temp;
+        }
+    }
+
+    return residual;
 }
 
-void tf_collection_create(tf_collection* tfc) {
-    tfc->tfs.clear();
-    delete tfc;
+float regression_end(precomp comp, tf_data& tf, int i, int j) { // prob only nec to look at corresp i j in domain, range?
+    float ds = (float) blockSum(domain);
+    float dsq = (float) blockSumofSq(domain);
+
+    // ... and so on
+
 }
 
 float regression_check(block* domain, block* range, int orient, tf_data& tf) {
@@ -69,86 +120,7 @@ float regression_check(block* domain, block* range, int orient, tf_data& tf) {
 
     float closed_form = tf.contrast * tf.contrast * dsq + 2*tf.contrast*tf.brightness*ds - 2*tf.contrast * sumdr + tf.brightness * tf.brightness * domain->size * domain->size - 2 * tf.brightness*rs + ((float)blockSumOfSq(range));
 
-    if (residual - closed_form > 1000) {
-        printf("%f, %f, makes %f\n", residual, closed_form, residual - closed_form);
-    }
+    printf("%f, %f, makes %f\n", residual, closed_form, residual - closed_form);
 
     return residual;
-}
-
-
-float regression(block* domain, block* range, int orient, tf_data& tf) {
-    return regression_check(domain, range, orient, tf);
-    //...
-    float ds = (float)blockSum(domain);
-    float dsq = (float)blockSumOfSq(domain);
-
-    float denom = domain->size * domain->size * dsq - ds * ds;
-
-    if(denom == 0) {
-        return 100000;
-    }
-
-
-    float xy = 0;
-    for(unsigned int i = 0; i < domain->size; i++) {
-        for(unsigned int j = 0; j < domain->size; j++) {
-            unsigned int y = i, x = j;
-            calc_pos(&y, &x, orient, domain->size);
-
-            xy += yx_to_val(y,x,domain) * yx_to_val(i,j,range);
-        }
-    }
-
-    float rs = (float)blockSum(range);
-
-    tf.contrast = ((float)(domain->size * domain->size * xy) - ds * rs)/denom;
-    /* tf.brightness = (float)(rs * dsq - ds * xy)/denom; */
-    tf.brightness = (float)(rs - tf.contrast * ds)/(domain->size*domain->size);
-    /* printf("%f %f\n", tf.contrast, tf.brightness); */
-    tf.d_block[0] = domain->y;
-    tf.d_block[1] = domain->x;
-    tf.r_block[0] = range->y;
-    tf.r_block[1] = range->x;
-    tf.size = range->size;
-    tf.orient = orient;
-
-    float residual = 0;
-
-    for(unsigned int i = 0; i < domain->size; i++) {
-        for(unsigned int j = 0; j < domain->size; j++) {
-            unsigned int y = i, x = j;
-            calc_pos(&y, &x, orient,domain->size);
-
-            float temp = (yx_to_val(y,x,domain) * tf.contrast + tf.brightness 
-                    - yx_to_val(i,j,range));
-            temp *= temp;
-            residual += temp;
-        }
-    }
-
-    return residual;
-}
-
-
-
-void print_encoding(tf_collection* tfc) {
-    FILE *encoding_file = fopen("encoding.txt", "w");
-    for(tf_data t : tfc->tfs) {
-        fprintf(
-                encoding_file,
-                "(%2d, %2d) -> (%2d %2d) "
-                "s:%4d o:%2d c:%+2.2f b:%+2.2f\n", 
-                t.d_block[0],
-                t.d_block[1],
-                t.r_block[0],
-                t.r_block[1],
-                t.size,
-                t.orient,
-                t.contrast,
-                t.brightness
-               );
-    }        
-    fclose(encoding_file);
-    debug("Success: encoding.txt created.\n");
 }
